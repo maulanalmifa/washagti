0900ab166e462b2e8ff4fae3dc43882f5ae65893
maulanalmifa <lanal.fahmi27@gmail.com>
2019-11-18 14:41:06 +0700

    Login berhasil

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('judul','Login Washagti')
+@section('content')
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+
+        <p>&larr; <a href="/">Home</a>
+
+        <h4>Masuk ke Washagti</h4>
+        <p>Belum punya akun? <a href="/register">Daftar di sini</a></p>
+
+        <form action="{{ route('login') }}" method="POST">
+        @csrf
+            <div class="form-group">
+                <label for="username">{{ __('Username') }}</label>
+                <input class="form-control @error('username') is-invalid @enderror" type="text" name="username" placeholder="Username" />
+                @error('username')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+
+            <div class="form-group">
+                <label for="password">{{ __('Password') }}</label>
+                <input class="form-control @error('password') is-invalid @enderror" type="password" name="password" placeholder="Password" />
+                @error('password')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
+            <input type="submit" class="btn btn-success btn-block" name="login" value="Masuk" />
+            @if (Route::has('password.request'))
+                <a class="btn btn-link" href="{{ route('password.request') }}">
+                    {{ __('Forgot Your Password?') }}
+                </a>
+            @endif
+        </form>
+            
+        </div>
+
+        <div class="col-md-6">
+            <!-- isi dengan sesuatu di sini -->
+        </div>
+
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,13 @@
 Route::get('/', function () {
     return view('home');
 });
+Route::get('/login','UserController@login');
+Route::get('register','UserController@regist');
 Route::get('estimasi','EstimateController@index');
 Route::get('blog','BlogController@index');
 Route::get('tentang','AboutController@index');
 Route::post('pesanan/tambah','PesananController@pesan');
 Route::get('pesanan/cek','PesananController@cek');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
